9a6f3a24f3e2e4d1c77c1356b34a636cbdd7da3a
GabrielBragaa <gbragamoura@hotmail.com>
2023-10-09 19:00:49 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(json());
 app.use(router);
 
 
-const port = 5000;
+const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+	console.log(`Servidor rodando na porta ${port}`)
 })
